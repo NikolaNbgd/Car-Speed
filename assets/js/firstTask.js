04ad77c77410b598e69a32ec0142cc69af695fea
NikolaNbgd <npopovic1992@yahoo.com>
2017-07-22 23:02:51 +0800

    rendering json data

--- assets/js/firstTask.js
+++ assets/js/firstTask.js
@@ -1,26 +1,30 @@
+
 //Variable in which we will append data
-var carBlocks = document.getElementById('cars');
+var car_blocks = document.getElementById('cars');
 
 //Ajax call
 var jsonRequest = new XMLHttpRequest();
-jsonRequest.open('POST', 'data.json');
-jsonRequest.onload = function () {
+jsonRequest.open('GET', 'data.json');
+jsonRequest.onreadystatechange = function () {
 	var ourData = JSON.parse(jsonRequest.responseText);
-	renderHTML(ourData);
+	//Call renderData function
+	renderData(ourData);
 }
+
 jsonRequest.send();
 
-	
-function renderHTML(data) {
+function renderData(data) {
 
 	//An empty string in which we will store data 
 	var carString = "";
 
 	//Loop for passing through all data
-	for (i = 0; i < data.length; i++) {
-		carString += '<p>'+data[i].cars.name+'</p>';
-	}
-
-	carBlocks.insertAdjacentHTML('beforeend',carString);
-
+	for (i = 0; i < data.cars.length; i++) {
+				var carString = '<div class="col-sm-4">';
+				carString += '<img src="'+data.cars[i].image+'">';
+				carString += '<p>'+data.cars[i].name+'</p>';
+				carString += '</div>';
+		}
+		
+	car_blocks.insertAdjacentHTML('beforeend', carString);
 }
