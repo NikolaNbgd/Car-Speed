affdab727745287ed254a007551cad83cbd6457e
NikolaNbgd <npopovic1992@yahoo.com>
2017-07-24 11:32:21 +0200

    First task - end

--- assets/js/firstTask.js
+++ assets/js/firstTask.js
@@ -2,11 +2,11 @@
 //Variable in which we will append data
 var car_blocks = document.getElementById('cars');
 
+//Static json.data Ajax call
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function() {
     if (xhr.readyState == XMLHttpRequest.DONE) {
     	var ourData = JSON.parse(xhr.responseText);
-        console.log(ourData);
         renderData(ourData);
     }
 
@@ -14,6 +14,7 @@ xhr.onreadystatechange = function() {
 xhr.open('GET', 'data.json', true);
 xhr.send(null);
 
+//Function which render json data into div with id cars
 function renderData(data) {
 
 	//An empty string in which we will store data 
@@ -23,11 +24,17 @@ function renderData(data) {
 	for (i = 0; i < data.cars.length; i++) {
 		carString += '<div class="col-sm-4 car">';
 		carString += '<a href="#">';
+		carString += '<div class="face">';
 		carString += '<img src="'+data.cars[i].image+'">';
 		carString += '<p>'+data.cars[i].name+'</p>';
-		carString += '</a>'
+		carString += '</div>';
+		carString +=  '<div class="back face center">';
+    	carString +=  '<p>This is nice for exposing more information about an image.</p>';
+  		carString += '</div>';
+		carString += '</a>';
 		carString += '</div>';
 	}
 
+	//Inserting data in HTML page
 	car_blocks.insertAdjacentHTML('beforeend', carString);
 }
