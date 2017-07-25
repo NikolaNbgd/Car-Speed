8573b0203f63a939a3204bd9e22d206dfa2be91e
NikolaNbgd <npopovic1992@yahoo.com>
2017-07-25 11:12:03 +0200

    Animation is over

--- assets/js/search.js
+++ assets/js/search.js
@@ -6,7 +6,7 @@
     elements = document.getElementsByTagName("P");
 
         for (var i = 0; i < elements.length; i++){
-
+            
         if (!pattern.test(elements[i].id.toLowerCase())) {
             elements[i].parentNode.style.display = 'none';
         } else {
