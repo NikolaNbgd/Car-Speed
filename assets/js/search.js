cb3d13ff3e316231366e0fd78e1980e23fbfef73
NikolaNbgd <npopovic1992@yahoo.com>
2017-07-25 11:11:19 +0200

    Animation is over

---
+++ assets/js/search.js
@@ -0,0 +1,18 @@
+ function filter() {
+    
+    term = document.getElementById("search").value.toLowerCase();
+
+    pattern = new RegExp(term);
+    elements = document.getElementsByTagName("P");
+
+        for (var i = 0; i < elements.length; i++){
+
+        if (!pattern.test(elements[i].id.toLowerCase())) {
+            elements[i].parentNode.style.display = 'none';
+        } else {
+            elements[i].parentNode.style.display = 'block';
+
+        }
+        
+    }
+}
