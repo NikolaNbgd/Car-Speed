67dc96b3fbc0f5c208974f5f31ffdccb82ee8124
NikolaNbgd <npopovic1992@yahoo.com>
2017-07-27 00:07:59 +0200

    Changing some elements

--- assets/js/fourthTaskQuery.js
+++ assets/js/fourthTaskQuery.js
@@ -1,19 +1,30 @@
 $(document).ready(function () {
 
-	$('.flip-container').find('img').click(function () {
-		$(this).css('height', '50px');
-		$('#show-data1').find('.scale:first').append(this);
-	})
+	$('.flip-container .back').find('.back-img').click(function (e) {
+		e.preventDefault();
+
+		//Appeding selected images
+		var picture = $(this).next().css('height', '50px');
+		$('#show-data1').find('.scale:first').append(picture);
+
+		//For better user experiance
+		$('html, body').animate({
+	        scrollTop: $("#show-data1").offset().top
+	    }, 1500);
+
+	});
 
 	$.getJSON('data.json', function (response) {
 
+		//Variable for distance value
 		var dist = response.distance;
 
+		//Creating a scale
 		var someDiv1 = '<div class="scale" style="width: '+dist+'px; height: '+dist+'px;"></div>';
 		var someDiv2 = '<div class="scale" style="width: '+dist+'px; height: '+dist+'px;"></div>';
 		var someDiv3 = '<div class="scale" style="width: '+dist+'px; height: '+dist+'px;"></div>';
 
-			for (var y = 0; y < 10; y++) {
+			for (var i = 0; i < 10; i++) {
 				$('#show-data1').append(someDiv1);
 				$('#show-data2').append(someDiv2);
 				$('#show-data3').append(someDiv3);
@@ -21,11 +32,17 @@ $(document).ready(function () {
 		
 	});
 
+	//Validate input field
 	$(document).on('click', '.start', function () {
 		var speed = $('#carSpeed').val();
-		if (isNaN(speed) || speed.length == " ") {
-			alert('Ovo polje je obavezno i uneta vrednost mora biti broj');
+		if ( isNaN(speed) || speed.length == "" ) {
+			$('.flash-message').fadeIn();
 		}
 	})
 
-})
+	$(document).on('click', '.close-message', function (e) {
+		e.preventDefault();
+		$(this).parent().fadeOut();
+	})
+
+});
