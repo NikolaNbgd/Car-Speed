f66579dffc4852391d46ca26af6fe86fd0369f10
NikolaNbgd <npopovic1992@yahoo.com>
2017-07-28 19:12:22 +0200

    Initial setup

--- assets/js/sixthTaskQuery.js
+++ assets/js/sixthTaskQuery.js
@@ -1,13 +1,16 @@
 $(document).ready(function () {
 
+	//Counder for appending cars in different field
+	fieldNumber = 1;
+
 	$('.flip-container .back').find('.back-img').click(function (e) {
 		e.preventDefault();
 
 		//Appeding selected images
 		var picture = $(this).next().clone();
 		picture.css('height', '50px');
-		$('#show-data1').find('.scale:first').append(picture);
-		
+		$('#show-data'+fieldNumber).find('.scale:first').append(picture);
+		fieldNumber+=1;
 
 		//For better user experiance
 		$('html, body').animate({
@@ -15,6 +18,31 @@ $(document).ready(function () {
 	    }, 1500);
 
 	});
+
+
+
+	var carSpeeds;
+	$.getJSON('data.json', function (response) {
+		$.each(response.cars, function (i,car) {
+			var carSpeeds = car.speed;
+		})
+	})
+	
+	var ss = JSON.stringify(carSpeeds);
+	console.log(ss);
+
+	field = 1;
+	$(document).on('click', '.start', function () {
+		var ss = $('#show-data' + field).find('.scale:first');
+		field+=1;
+			if (ss.has('img')) {
+				for (var i = 0; i < ss.length; i++) {
+					console.log(ss.find('img').addClass('move'));
+				}
+			}
+	});
+
+
 
 	$.getJSON('data.json', function (response) {
 
@@ -35,12 +63,12 @@ $(document).ready(function () {
 	});
 
 	//Validate input field
-	$(document).on('click', '.action .start', function () {
-		var speed = $('#carSpeed').val();
-		if ( isNaN(speed) || speed.length == "" ) {
-			$('.flash-message').fadeIn();
-		}
-	});
+	// $(document).on('click', '.action .start', function () {
+	// 	var speed = $('#carSpeed').val();
+	// 	if ( isNaN(speed) || speed.length == "" ) {
+	// 		$('.flash-message').fadeIn();
+	// 	}
+	// });
 
 	$(document).on('click', '.close-message', function (e) {
 		e.preventDefault();
